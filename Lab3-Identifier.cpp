69be1ce882f882c9ce194946b970be95c23ff5f9
Chandon21 <132583061+Chandon21@users.noreply.github.com>
2024-02-23 23:04:42 +0600

    Add files via upload

---
+++ Lab3-Identifier.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isValid(string str, int n)
+
+{
+
+    if (!((str[0] >= 'a' && str[0] <= 'z')
+
+          || (str[0] >= 'A' && str[0] <= 'Z')
+
+          || str[0] == '_'))
+
+        return false;
+
+    for (int i = 1; i < str.length(); i++) {
+
+        if (!((str[i] >= 'a' && str[i] <= 'z')
+
+              || (str[i] >= 'A' && str[i] <= 'Z')
+
+              || (str[i] >= '0' && str[i] <= '9')
+
+              || str[i] == '_'))
+
+            return false;
+
+    }
+
+    return true;
+
+}
+
+int main()
+
+{
+
+    string str;
+
+    cout<<"Enter an identifier "<<endl;
+
+    cin>>str;
+
+    int n = str.length();
+
+    if (isValid(str, n))
+
+        cout << "Valid Identifier";
+
+    else
+
+        cout << "Invalid Identifier";
+
+    return 0;
+
+}
